5c11fe5f18b3be125487efdbd3a71d64b9e66cea
cvalle19 <valleamcd@gmail.com>
2025-05-06 13:54:11 -0500

    first commit

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <div className={`${bebas.className} flex flex-row items-center leading-none text-white`}>
       <FaReact className="h-20 w-20 rotate-[15deg]" />
-      <p className="text-[30px] ml-3">AnjrotDev</p>
+      <p className="text-[30px] ml-3">APP EmyCaren</p>
     </div>
   );
 };
